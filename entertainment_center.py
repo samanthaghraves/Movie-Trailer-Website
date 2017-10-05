158bd4939e097caf7c396c8531415c0cfe4ef166
Samantha Vealli <samanthavealli@icloud.com>
2017-10-04 17:00:41 -0700

    Updates
    Got my review back, made modifications, now uploading FGJ

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,10 @@
 import media
 import fresh_tomatoes
 
+'''The information that is displayed on the website. In order to make a movie,
+you simply create an instance of a Movie, then add the arguments:
+(name, summary, poster image, and youtube URL). '''
+
 big_money_hustlas = media.Movie("Big Money Hustlas",
                                 "Jugalos murder each other and other people",
                                 "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkyOTM5MjU0Nl5BMl5BanBnXkFtZTcwOTE3MDAzMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
@@ -22,7 +26,8 @@ battle_royale = media.Movie("Battle Royale",
                             "https://www.youtube.com/watch?v=N0p1t-dC7Ko")
 
 bojack_horseman = media.Movie("Bojack Horseman",
-                              "A horseman who is awful has terrible things happen to him",
+                              '''A horseman who is awful has terrible
+                              things happen to him''',
                               "http://i.imgur.com/uQD2aN6.jpg",
                               "https://www.youtube.com/watch?v=Bf12qwPWDVI")
 
@@ -31,8 +36,11 @@ matilda = media.Movie("Matilda",
                       "http://img.moviepostershop.com/matilda-movie-poster-1996-1010484797.jpg",
                       "https://www.youtube.com/watch?v=MdC_YMvYZyI")
 
-
+'''This is the list that tells fresh_tomatoes which movies to show.
+If we take away or add to this list, it will still work, it will
+just show less or more movies respectively. '''
 movies = [big_money_hustlas, toy_story, school_of_rock, battle_royale, bojack_horseman, matilda]
+
+'''Calls fresh_tomatoes to load the webpage'''
 fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-#print(media.Movie.__doc__)
+
