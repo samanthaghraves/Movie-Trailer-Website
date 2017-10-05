e33d31d2ad6870aea0e6846704989c9dff4bc989
Samantha Vealli <samanthavealli@icloud.com>
2017-10-05 11:28:22 -0700

    Updated
    Made noted changes for Pep-8 and documentation for class Movie.
    
    Should be good here

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,22 +7,22 @@ you simply create an instance of a Movie, then add the arguments:
 
 big_money_hustlas = media.Movie("Big Money Hustlas",
                                 "Jugalos murder each other and other people",
-                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkyOTM5MjU0Nl5BMl5BanBnXkFtZTcwOTE3MDAzMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+                                "https://images-na.ssl-images-amazon.com/images/M/MV5BMTkyOTM5MjU0Nl5BMl5BanBnXkFtZTcwOTE3MDAzMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",  # noqa
                                 "https://www.youtube.com/watch?v=LIy6GQaMmWE")
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",
+                        "http://vignette4.wikia.nocookie.net/disney/images/4/4c/Toy-story-movie-posters-4.jpg/revision/latest?cb=20140816182710",  # noqa
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 school_of_rock = media.Movie("School of Rock",
                              "Dude steals kids and makes an awesome band",
-                             "https://images-na.ssl-images-amazon.com/images/I/51v8TQDTF-L._SY300_.jpg",
+                             "https://images-na.ssl-images-amazon.com/images/I/51v8TQDTF-L._SY300_.jpg",  # noqa
                              "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
 battle_royale = media.Movie("Battle Royale",
                             "The original Hunger Games",
-                            "https://i1.wp.com/thefoxisblack.com/blogimages//Keorattana-Luangrathrajasombat-Battle-Royale-Poster.jpg?resize=800%2C1200",
+                            "https://i1.wp.com/thefoxisblack.com/blogimages//Keorattana-Luangrathrajasombat-Battle-Royale-Poster.jpg?resize=800%2C1200",  # noqa
                             "https://www.youtube.com/watch?v=N0p1t-dC7Ko")
 
 bojack_horseman = media.Movie("Bojack Horseman",
@@ -33,14 +33,14 @@ bojack_horseman = media.Movie("Bojack Horseman",
 
 matilda = media.Movie("Matilda",
                       "Autistic girl has magical powers and is awesome",
-                      "http://img.moviepostershop.com/matilda-movie-poster-1996-1010484797.jpg",
+                      "http://img.moviepostershop.com/matilda-movie-poster-1996-1010484797.jpg",  # noqa
                       "https://www.youtube.com/watch?v=MdC_YMvYZyI")
 
 '''This is the list that tells fresh_tomatoes which movies to show.
 If we take away or add to this list, it will still work, it will
 just show less or more movies respectively. '''
-movies = [big_money_hustlas, toy_story, school_of_rock, battle_royale, bojack_horseman, matilda]
+movies = [big_money_hustlas, toy_story, school_of_rock, battle_royale,
+          bojack_horseman, matilda]
 
 '''Calls fresh_tomatoes to load the webpage'''
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,14 +1,19 @@
 import webbrowser
 
+
 class Movie():
-    """ This class provides a way to store movie related information """
-    
-    VALID_RATINGS = ["G", "PG", "PG-13", "R", "NC-17"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, youtube_url):
+    """ This class provides a way to store movie related information
+        and is called by movie_name=movie.Movie. It takes the arguments
+        of self, movie_title, movie_storyline, poster_image, and
+        youtube_url. The result is a movie poster that shows a
+        movie trailer when clicked after fresh_tomatoes makes the
+        HTML file."""
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, youtube_url):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = youtube_url
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
